dc646ac60b9bd3b46a557009e998a94e9c97349b
Harald Gültig <hgueltig@gmail.com>
2019-02-16 00:26:16 +0100

    GUI changes

--- GUI/docroot/js/pickstrategie.js
+++ GUI/docroot/js/pickstrategie.js
@@ -1,26 +1,33 @@
 var pickstrategy_canvas = document.getElementById('pickstrategy-canvas');
 var pickstrategy_context;
 
-var balls;
+var balls, cols, rows;
 var collided_id, old_collided_id;
+var collided_row, collided_column;
 var mouse_down = false;
 
-function drawWells(cols, rows, colonys = 1)
+function drawWells(col_arg, rows_arg, colonys = 1)
 {
+	// TODO default starting well
+	cols = col_arg;
+	rows = rows_arg;
+	let size;
+	if(cols >= 20) size = 26;
+	else size = 32;
 	pickstrategy_context = pickstrategy_canvas.getContext('2d');
 	pickstrategy_context.clearRect(0, 0, pickstrategy_canvas.width, pickstrategy_canvas.height);
 	collided_id = -1, old_collided_id = -1;
 	balls = [];
-	pickstrategy_context.canvas.width = cols*32+50;
-	pickstrategy_context.canvas.height = rows*32+50;
+	pickstrategy_context.canvas.width = cols*size+50;
+	pickstrategy_context.canvas.height = rows*size+55+20;
 
 	for (var x = 0; x < cols; ++x){
 		for (var y = 0; y < rows; ++y)
 		{
 			ball = new Circle({
-				x: x *32 + 60,
-				y: y *32 + 65,
-				radius: 14,
+				x: x * size + 60,
+				y: y * size + 65,
+				radius: size/2-2,
 				background: 'white',
 				canvas: pickstrategy_canvas
 			});
@@ -39,10 +46,10 @@ function drawWells(cols, rows, colonys = 1)
 	pickstrategy_context.font = "16px Arial";
 	pickstrategy_context.fillStyle = "red";
 	for (var i = 0; i < cols; ++i) {
-		pickstrategy_context.fillText(i+1, 50 +8 +32 *i, 30);
+		pickstrategy_context.fillText(i+1, 50 + size *i, 30);
 	}
 	for (var i = 0; i < rows; ++i) {
-		pickstrategy_context.fillText(String.fromCharCode(65 +i), 10, 32 * i + 70);
+		pickstrategy_context.fillText(String.fromCharCode(65 +i), 10, size * i + 70);
 	}
 
 }
@@ -81,6 +88,14 @@ pickstrategy_canvas.addEvent('mousemove', function (e)
 				balls[i].set('background', (i < collided_id ? 'white' : 'green'));
 			}
 			old_collided_id = collided_id;
+			
+			pickstrategy_context.clearRect(0, pickstrategy_context.canvas.height-20, pickstrategy_context.canvas.width, 20); 
+			pickstrategy_context.fillStyle = "black";
+			collided_row = collided_id%rows;
+			collided_column = (collided_id-collided_id%rows)/rows;
+			console.log({collided_row});
+			console.log({collided_column});
+			pickstrategy_context.fillText("Start at well: "+String.fromCharCode(65 + collided_row)+(collided_column+1),0, pickstrategy_context.canvas.height);
 		}
 	} else return false;
 });
@@ -91,5 +106,4 @@ pickstrategy_canvas.addEvent('mousedown', function (e){
 });
 pickstrategy_canvas.addEvent('mouseup', function (e){
 	mouse_down = false;
-	console.log("Starting Well: ", collided_id+1);
 });
