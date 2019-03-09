ef210969c23ac0418151529a2929fe85904f72e4
Oliver Tale-Yazdi <oliver.tale@web.de>
2019-03-09 23:49:48 +0100

    Fix #36

---
+++ Main/resource_path.cc
@@ -0,0 +1,68 @@
+#include "include/resource_path.h"
+#include "include/exception.h"
+
+namespace c3picko
+{
+ResourcePath::ResourcePath(QString system_absolute_path) : system_absolute_(system_absolute_path) {}
+
+ResourcePath::ResourcePath() {}
+
+ResourcePath ResourcePath::fromSystemAbsolute(QString path) { return path; }
+
+ResourcePath ResourcePath::fromServerAbsolute(QString path) { return Root() + path; } // We have two // now, but the operator+ manages that
+
+ResourcePath ResourcePath::fromDocRootAbsolute(QString path) { return DocRoot() + path; }
+
+const QString ResourcePath::toSystemAbsolute() const
+{
+	if (system_absolute_.isEmpty())
+		throw Exception("Empty path");
+	return system_absolute_;
+}
+
+const QString ResourcePath::toServerAbsolute() const
+{
+	QString root_abs = Root().toSystemAbsolute();
+
+	if (!system_absolute_.startsWith(root_abs))
+		throw Exception("System path outside of Root can not be converted to 'ServerAbsolute'");
+
+	return system_absolute_.mid(root_abs.size() - 1);
+}
+
+const QString ResourcePath::toDocRootAbsolute() const
+{
+	QString droot_abs = DocRoot().toSystemAbsolute();
+
+	if (!system_absolute_.startsWith(droot_abs))
+		throw Exception("System path outside of DocRoot can not be converted to 'DocRootAbsolute'");
+
+	return system_absolute_.mid(droot_abs.size() - 1);
+}
+
+bool ResourcePath::exists() const { return QFileInfo(system_absolute_).exists(); }
+
+bool ResourcePath::isDir() const { return QFileInfo(system_absolute_).isDir(); }
+
+bool ResourcePath::isFile() const { return QFileInfo(system_absolute_).isFile(); }
+
+bool ResourcePath::isEmpty() const { return system_absolute_.isEmpty(); }
+
+void ResourcePath::clear() { system_absolute_ = ""; }
+
+ResourcePath ResourcePath::operator+(const QString& obj) const { return QString(system_absolute_ + obj).replace("//", "/"); }
+
+template <> ResourcePath Marshalling::fromJson(const QJsonObject& obj)
+{
+	return ResourcePath::fromServerAbsolute(obj["server_absolute"].toString());
+}
+
+template <> QJsonObject Marshalling::toJson(const ResourcePath& value)
+{
+	QJsonObject obj;
+
+	obj["server_absolute"] = value.toServerAbsolute();
+
+	return obj;
+}
+}
