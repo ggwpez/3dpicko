d1e42ccbc32d789344088d261dbd3578a9ef8926
Patrick Mayer <patrick.mayer@posteo.de>
2019-03-25 15:58:52 +0100

    changed nav and error correction

--- GUI/docroot/js/wiki.js
+++ GUI/docroot/js/wiki.js
@@ -2,11 +2,52 @@
 
 //loads front page#
 $(function(){
-  $("#wiki-content").load("./docs/wiki.html #introduction"); 
+  $("#wiki-content").load("./docs/wiki.html #introduction");
+  var element = document.getElementById("Introduction");
+  element.classList.remove("btn-link");
+  element.classList.add("btn-primary");
 });
 
-function loadWiki(site){
+function loadWiki(id, site){
     $("#wiki-content").load(site);
+    resetButtons();
+    var element = document.getElementById(id);
+    element.classList.remove("btn-link");
+    element.classList.add("btn-primary");
+}
+
+function resetButtons(){
+    var element = document.getElementById("Introduction");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+    
+    var element = document.getElementById("User-Manual-First-Steps");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+    
+    var element = document.getElementById("User-Manual-Example-Picking-Process");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+    
+    var element = document.getElementById("User-Manual-Advanced-Settings");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+    
+    var element = document.getElementById("Configuration-Hardware");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+    
+    var element = document.getElementById("Configuration-Software");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+
+    var element = document.getElementById("Administration-Hardware");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
+    
+    var element = document.getElementById("Administration-Software");
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-link");
 }
 
 function loadHelp(headline, site){
