09d6184c1c29d81ebe3d6242e12867ece9816de3
Oliver Tale-Yazdi <oliver.tale@web.de>
2019-03-27 00:19:45 +0100

    Fixup

---
+++ GUI/include/types/history.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "include/types/job.hpp"
+
+namespace c3picko {
+/**
+ * @brief Holds all information about the exucution of a pick job.
+ */
+class History {
+ private:
+  Job::ID job_;
+};
+}  // namespace c3picko
