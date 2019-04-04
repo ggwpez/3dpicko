d7012f6f7f5bd9464ef43ca5e5fc2ef03b5f466a
Oliver Tale-Yazdi <oliver.tale@web.de>
2019-04-04 03:22:15 +0200

    Adding restart

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=.
-./main serverconfig.ini $*
+
+while : ; do
+	./main serverconfig.ini $*
+	# 101 is the hardcoded restart exit code
+	[[ $? -eq 101 ]] || break
+done
 
 exit 0
