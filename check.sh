1cf1f703154e1e4e95bc39ca60d25504dcbb1137
Oliver Tale-Yazdi <oliver@tasty.limo>
2020-07-19 18:34:47 +0200

    Trying out ci…

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
 
 #/bin/bash
 
+clang-format --version
 [ -z "$(find Main/src Main/include -regex '.*\.\(cc\|h\|hpp\|cpp\)' -exec clang-format -style=file {} \;)" ] || (echo '--- C++ Format error' && false)
 [ -z '$(find . -name "*.html" -type f -exec xmllint --noout --html --format "{}" \;)' ] || (echo '--- HTML Format error' && false)
